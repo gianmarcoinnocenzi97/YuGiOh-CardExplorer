3bc39caf6e6715f5fea8ceee0e2ada931c67bb32
Gianmarco Innocenzi <innocenzi.gian@gmail.com>
2025-09-13 17:35:47 +0200

    Primo commit

---
+++ src/main/java/com/ygo/YgoApplication.java
@@ -0,0 +1,13 @@
+package com.ygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygo/YgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
